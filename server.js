5f87bc3a3d0150f2c61e7791722f69f307cfa73e
niccentury <cjdcec@gmail.com>
2018-09-19 11:10:44 -0700

    finished doing callbacks

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 'use strict';
 
-const pg = require('pg');
 const express = require('express');
 const ejs = require('ejs');
 require('dotenv').config()
-
+const bookfetch = require('./bookfetch')
 const PORT = process.env.PORT;
 const app = express();
-
-const conString = process.env.DATABASE_URL;
-const client = new pg.Client(conString);
-client.connect();
-client.on('error', error => {
-  console.error(error);
-});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -26,27 +18,9 @@ app.get('/hello', (request, response) => {
   response.render('hello.ejs', {root: './public'});
 });
 
-app.get('/books', (request, response) => {
-  client.query('SELECT * FROM books;')
-    .then( (result) => {
-      response.render('index', {
-        books: result.rows
-      })
-    })
-})
+app.get('/books', bookfetch.getBooks);
 
-app.get('/books/:id', (request, response) =>{
-  let SQL = 'SELECT * FROM books WHERE id = $1';
-  let values = [ request.params.id ]
-  client.query(SQL, values, (err, result) => {
-    if(err){
-      console.log(err);
-      response.redirect('/error');
-    } else {
-      response.render('show', {book: result.rows[0]});
-    }
-  })
-})
+app.get('/books/:id', bookfetch.getOneBook);
 
 app.get('*', (request, response) => {
   response.statusCode = 404
